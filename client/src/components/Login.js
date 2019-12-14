cd42d2035ac29d057c0d2b215c846d49436e5d06
Jay Leach <42565053+leachcoding@users.noreply.github.com>
2019-12-13 22:16:40 -0500

    stretch mvp done

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from '../utils/api'
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [form, setForm] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = e => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = () => {
+    api().post('/api/login', form)
+      .then(response => {
+        console.log(response);
+        localStorage.setItem('token', response.data.payload);
+        props.history.push('/bubbles');
+      })
+      .catch(error => console.error('error', error))
+  }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <form onSubmit={(e) => {
+            e.preventDefault()
+            handleSubmit()
+        }}>
+            <input
+                type='text'
+                name='username'
+                placeholder='User Name'
+                value={form.name}
+                onChange={handleChange}
+            />
+            <input
+                type='password'
+                name='password'
+                placeholder='Password'
+                value={form.password}
+                onChange={handleChange}
+            />
+           <button type='submit'>Login</button>
+        </form>
   );
 };
 
